6a804303d42529840afa8fbd2a64edfe81504eea
Jorge Garcia <garciajorgecf@gmail.com>
2025-05-22 22:41:04 -0600

    Añadiendo funcionalidad de GetPayments del microservicio Pagos

--- pagos/pagos.go
+++ pagos/pagos.go
@@ -11,6 +11,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Asi es la estructura de los pagos basado en el json de job_bank del microservicio de simulador
+type Pago struct {
+	SSN             string  `json:"ssn"` // Numero de seguro social del cliente
+	Amount          float64 `json:"amount"` // Monto del pago
+	TransactionType string  `json:"transaction_type"` // Tipo de transacción
+	Status          string  `json:"status"` // Estado de la transacción
+	CreationDate    string  `json:"creation_date"` // Fecha de creación del pago
+	TransactionID   string  `json:"transaction_id"` // ID de la transacción
+	Source          string  `json:"source"` // Fuente del pago
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", Index)
@@ -23,25 +34,31 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetPayments(w http.ResponseWriter, r *http.Request) {
-	/*
-		Agrega aquí la lógica de negocio del microservicio para realizar
-		lo siguiente:
-		- obtener los pagos de pólizas realizados por los clientes
+	// Se obtiene el archivo en la ruta donde se genera el json segun el job_bank.py
+	contenidoArchivo, e := ioutil.ReadFile("/data/payment_records.json")
+	if e != nil {
+		log.Printf("No se pudo leer el archivo: %v", e)
+		http.Error(w, "Error leyendo el archivo json", http.StatusInternalServerError)
+		return
+	}
 
-		Cada registro deberá contener la siguiente información:
-		- "amount": monto del pago realizado
-		- "transaction_type": canal digital utilizado para realizar el pago
-		- "status": estatus del pago
-		- "creation_date": fecha en la que se realizó el pago
-		- "transaction_id": identificador de la transacción
-		- "source": origen del pago
+	// Como no se tiene forma de json correcta se mapea el json a un mapa
+	var datosJSON map[string][]Pago
+	if e := json.Unmarshal(contenidoArchivo, &datosJSON); e != nil {
+		log.Printf("Error de mapeo de los datos JSON: %v", e)
+		http.Error(w, "No se pudo dar formato al archivo de los pagos de clientes", http.StatusInternalServerError)
+		return
+	}
 
-		Nota: en la implementación provista del sistema se simula el proceso de pago de
-		póliza por lo que NO ES NECESARIO realizar algún tipo de conexión a servicios
-		de pagos digitales reales. Para consultar la información de los pagos que los
-		clientes asegurados han realizado e IMPLEMENTAR ESTE MICROSERVICIO, deberás
-		CONSULTAR EL ARCHIVO 'payment_records.json' GENERADO POR EL COMPONENTE SIMULADOR.
-	*/
-	w.WriteHeader(500)
-	fmt.Fprintf(w, "%q not implemented yet", html.EscapeString(r.URL.Path))
+	// Como se tiene el numero de seguro social aparte, se añade como un atributo al pago
+	for ssn, payments := range datosJSON {
+		for iteradorPagos := range payments {
+			payments[iteradorPagos].SSN = ssn
+		}
+		datosJSON[ssn] = payments
+	}
+
+	// Una vez procesados los datos se envia todo el mapa formateado de nuevo como un archivo con formato JSON con los pagos en orden
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(datosJSON)
 }
